81a3d3784a26a166467d4b047db0905e8025fa64
Klein Lavina <160082336+KleinLavina@users.noreply.github.com>
2025-02-21 22:53:04 +0800

    Delete KleinShop directory

--- KleinShop/elements/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-      <li><a href="?page=home">Home</a></li>
-      <li><a href="?page=products">Products</a></li>
-      <li><a href="?page=login">Log In</a></li>
-    </ul>
-  </nav>
