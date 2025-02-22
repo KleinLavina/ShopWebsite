7f5a800bdb30b4d7c3cd955fc0a00edaf5e0a30a
Klein Lavina <160082336+KleinLavina@users.noreply.github.com>
2025-02-22 08:41:48 +0800

    Add files via upload

---
+++ KleinShop/elements/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+      <li><a href="?page=home">Home</a></li>
+      <li><a href="?page=products">Products</a></li>
+      <li><a href="?page=login">Log In</a></li>
+      <li><a href="?page=signup">Sign Up</a></li>
+    </ul>
+  </nav>
---
+++ KleinShop/pages/home.php
@@ -0,0 +1,4 @@
+<div class="contents">
+<h2>Welcome to My Website</h2>
+<p>This is a simple website with a navigation menu and hover effects.</p>
+</div>
